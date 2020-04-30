a4b0190f86938af5fd91b1762f38067bdb2a06e4
Mezbaul Islam Protick <mezbaulislamprotick@outlook.com>
2020-04-30 08:32:05 +0600

    Designed Favourite Counter

--- src/_elements/Header.jsx
+++ src/_elements/Header.jsx
@@ -1,37 +1,67 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className="navbar-fixed">
-        <nav>
-          <div className="nav-wrapper deep-purple darken-4">
-            <NavLink exact={true} to="/" className="brand-logo myLogo">
-              <span className="myLogo__up">B</span>
-              <span className="myLogo__down">B</span>
-            </NavLink>
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <NavLink exact={true} to="/">
-                  HOME
-                </NavLink>
-              </li>
-              <li className="favnav">
-                <NavLink exact={true} to="/favourite">
-                  FAVOURITE
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+    render() {
+        let countFav;
+
+        if (this.props.favourite.length > 0) {
+            countFav = (
+                <span id="countFav__inner">
+                    {" "}
+                    {this.props.favourite.length}{" "}
+                </span>
+            );
+        } else {
+            countFav = <span> </span>;
+        }
+        return (
+            <div className="navbar-fixed">
+                <nav>
+                    <div className="nav-wrapper deep-purple darken-4">
+                        <NavLink
+                            exact={true}
+                            to="/"
+                            className="brand-logo myLogo"
+                        >
+                            <span className="myLogo__up">B</span>
+                            <span className="myLogo__down">B</span>
+                        </NavLink>
+                        <ul className="right hide-on-med-and-down">
+                            <li>
+                                <NavLink exact={true} to="/">
+                                    HOME
+                                </NavLink>
+                            </li>
+                            <li className="favnav">
+                                <NavLink
+                                    exact={true}
+                                    to="/favourite"
+                                    id="countFav"
+                                >
+                                    <span>FAVOURITE</span>
+                                    {countFav}
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    const { beers, favourite } = state.beers;
+    return {
+        beers,
+        favourite,
+    };
+}
+
+export default connect(mapStateToProps)(Header);
